a499d2cdaa6f9b5d46c65ba22de3218dd09277da
balumalladi <baluchowdary8014@gmail.com>
2021-04-07 23:22:04 -0500

    added

--- controllers/snakes.js
+++ controllers/snakes.js
@@ -1,21 +1,27 @@
 var snakes = require('../models/snakes');
-// List of all Costumes
-exports.snakes_list = function(req, res) {
-res.send('NOT IMPLEMENTED: snakes list');
-};
-// for a specific Costume.
+// List of all snakes
+exports.snakes_list = async function(req, res) {
+    try{
+    thesnakes = await snakes.find();
+    res.send(thesnakes);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
+// for a specific snakes.
 exports.snakes_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: snakes detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
+// Handle snakes create on POST.
 exports.snakes_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: snakes create POST');
 };
-// Handle Costume delete form on DELETE.
+// Handle snakes delete form on DELETE.
 exports.snakes_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: snakes delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle snakes update form on PUT.
 exports.snakes_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: snakes update PUT' + req.params.id);
 };
